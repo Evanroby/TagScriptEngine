3202e7e45d8d3d68aac918bef3e2a15664be9147
Cool aid <84676543+cool-aid-man@users.noreply.github.com>
2026-03-25 00:33:19 +0530

    TSE: Update parser limit from 2k to 10k allowing full length embeds
    - Embeds now support 6k total characters.
    - Embed-description now supports 4k characters.
    - Both embed block and JSON now supports it

--- TagScriptEngine/interpreter.py
+++ TagScriptEngine/interpreter.py
@@ -208,7 +208,7 @@ class _Interpreter(Protocol):
         response: Response,
         *,
         charlimit: int,
-        verb_limit: int = 2000,
+        verb_limit: int = 10000,
         dot_parameter: bool,
     ) -> str: ...
 
@@ -442,7 +442,7 @@ class AsyncInterpreter(Interpreter):
         response: Response,
         *,
         charlimit: int,
-        verb_limit: int = 2000,
+        verb_limit: int = 10000,
         dot_parameter: bool,
     ) -> str:
         final = message
